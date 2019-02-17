ac50cab43680682f84e92e0a3f9097c99a12085c
Seita Higashi <higashi110902@gmail.com>
2019-02-18 00:01:15 +0900

    this is run

--- lib/ElixirInterpreter/core.ex
+++ lib/ElixirInterpreter/core.ex
@@ -1,10 +1,11 @@
 defmodule ElixirInterpreter.Core do
   def to_function(str) when is_binary(str) do
-    [ order | arg ] = str |> String.split
+    [ order | args ] = str |> String.split
     { func, module } = order
     |> String.split(".")
-    |> List.popat(-1)
+    |> List.pop_at(-1)
     module = Module.concat(module)
-    func = Function.capture(module, String.to_atom(func), 1)
+    func = String.to_atom(func)
+    apply(module, func, args)
   end
 end
